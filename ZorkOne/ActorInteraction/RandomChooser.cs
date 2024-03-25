0268467a6c542485b320e214706d29c86586696b
Michael Lane <hello@newzork.ai>
2024-03-24 21:23:03 -0500

    Prepare for combat unit testing.

---
+++ ZorkOne/ActorInteraction/RandomChooser.cs
@@ -0,0 +1,13 @@
+using ZorkOne.Interface;
+
+namespace ZorkOne.ActorInteraction;
+
+internal class RandomChooser : IRandomChooser
+{
+    private readonly Random _rand = new();
+
+    public T Choose<T>(List<T> items)
+    {
+        return items[_rand.Next(items.Count)];
+    }
+}
