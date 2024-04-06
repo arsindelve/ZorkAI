d564c7b00179f8219193c420a93240b56506fb6b
Michael Lane <hello@newzork.ai>
2024-04-06 11:44:15 -0500

    WIP

--- API/Program.cs
+++ API/Program.cs
@@ -4,8 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,30 +18,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-GameEngine<ZorkI, ZorkIContext> CreateEngine()
-{
-    return new GameEngine<ZorkI, ZorkIContext>();
-}
-
-app.MapGet("/zorkOne/respond", async () =>
-{
-    var database = new SessionRepository();
-    var sessionId = Environment.MachineName;
-    var savedGame = await database.GetSession(sessionId);
-    var engine = CreateEngine();
-
-    if (!string.IsNullOrEmpty(savedGame))
-    {
-        var decodedBytes = Convert.FromBase64String(savedGame);
-        var decodedText = Encoding.UTF8.GetString(decodedBytes);
-        engine.RestoreGame(decodedText);
-    }
-
-    return await engine.GetResponse("look");
-    
-})
-    .WithName("GetGameResponse")
-    .WithOpenApi();
 
 app.Run();
