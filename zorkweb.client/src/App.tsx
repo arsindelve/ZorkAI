63a46b255de1425f7930253efc5bfd084c4e035b
Michael Lane <mike@arsindelve.com>
2024-04-13 14:56:38 -0500

    WIP

--- zorkweb.client/src/App.tsx
+++ zorkweb.client/src/App.tsx
@@ -10,15 +10,16 @@ function App() {
         <div className="bg-[url('./src/assets/black-groove-stripes-repeating-background.jpg')] bg-repeat">
             <div className="flex flex-col min-h-screen">
                 <div className="flex-grow">
-                   
+
                     <QueryClientProvider client={queryClient}>
                         <Game/>
                     </QueryClientProvider>
 
                 </div>
                 <footer className="bg-gray-200 py-2">
-                    <p className={"text-center"}><a target="_blank" href="https://github.com/arsindelve/ZorkAI">Created
-                        By Michael Lane. Check out the repo here.</a></p>
+                    <p className={"text-center"}>Created
+                        By Mike in Dallas. <a target="_blank" href="https://github.com/arsindelve/ZorkAI">Check out the
+                            repo here.</a></p>
                 </footer>
             </div>
         </div>
