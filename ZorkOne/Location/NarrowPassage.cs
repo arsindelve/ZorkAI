7421fa6c38b8ff69122f176340fa0c3d3ba4b696
Michael Lane <mike@arsindelve.com>
2024-04-14 14:39:44 -0500

    Some fixes. Add Application Insights.

--- ZorkOne/Location/NarrowPassage.cs
+++ ZorkOne/Location/NarrowPassage.cs
@@ -2,7 +2,11 @@ namespace ZorkOne.Location;
 
 public class NarrowPassage : DarkLocation
 {
-    protected override Dictionary<Direction, MovementParameters> Map { get; }
+    protected override Dictionary<Direction, MovementParameters> Map =>
+        new()
+        {
+            { Direction.N, new MovementParameters { Location = GetLocation<RoundRoom>() } }
+        };
 
     protected override string ContextBasedDescription =>
         "This is a long and narrow corridor where a long north-south passageway briefly narrows even further. ";
