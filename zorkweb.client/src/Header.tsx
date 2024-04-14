6a337752a457553aadc69ea0df91d5a2d4ff1061
Michael Lane <hello@newzork.ai>
2024-04-13 19:55:02 -0500

    WIP

--- zorkweb.client/src/Header.tsx
+++ zorkweb.client/src/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderComponentProps {
 }
 
 const HeaderComponent: React.FC<HeaderComponentProps> = ({ locationName, moves, score }) => (
-    <div className="grid items-center bg-gray-900 p-3 text-white grid-cols-7 ">
+    <div className="grid items-center bg-sky-950 p-3 text-white grid-cols-7 ">
         <div className="col-span-5" >{locationName}</div>
         <div>Moves: {moves}</div>
         <div>Score: {score}</div>
