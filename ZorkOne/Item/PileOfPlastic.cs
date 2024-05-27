8938295d6b9fb4b0c8535a4e7dc8feb2698a44f0
michael <mike@arsindelve.com>
2024-05-26 19:24:33 -0700

    A few more little fixes

--- ZorkOne/Item/PileOfPlastic.cs
+++ ZorkOne/Item/PileOfPlastic.cs
@@ -4,19 +4,36 @@ using Model.Interface;
 
 namespace ZorkOne.Item;
 
-public class PileOfPlastic : ItemBase, ICanBeTakenAndDropped
+public class PileOfPlastic : ContainerBase, ICanBeTakenAndDropped, ICanHoldItems
 {
+    private List<IItem> _sharpItems =
+    [
+        Repository.GetItem<Sword>(),
+        Repository.GetItem<Knife>(),
+        Repository.GetItem<Trident>(),
+        Repository.GetItem<Sceptre>()
+    ];
+
     public bool IsInflated { get; set; }
 
     public override string[] NounsForMatching => ["pile", "pile of plastic", "plastic", "boat", "raft"];
 
-    public override string InInventoryDescription => "A pile of plastic";
+    public override string InInventoryDescription => IsInflated ? "A magic boat here. " : "A pile of plastic";
 
     public override int Size => 14;
 
-    public string OnTheGroundDescription => "There is a folded pile of plastic here which has a small valve attached. ";
+    public string OnTheGroundDescription => IsInflated
+        ? "There is a magic boat here. " + (Items.Any() ? ItemListDescription("magic boat") : "")
+        : "There is a folded pile of plastic here which has a small valve attached. ";
 
     public override string NeverPickedUpDescription => OnTheGroundDescription;
+
+    public override bool IsTransparent => true;
+
+    public override void Init()
+    {
+        // Starts empty 
+    }
 
     public override InteractionResult RespondToSimpleInteraction(SimpleIntent action, IContext context,
         IGenerationClient client)
@@ -27,6 +44,15 @@ public class PileOfPlastic : ItemBase, ICanBeTakenAndDropped
                 return new PositiveInteractionResult("The boat must be on the ground to be inflated. ");
 
             return new PositiveInteractionResult("You don't have enough lung power to inflate it. ");
+        }
+
+        if (action.Match(["deflate"], NounsForMatching))
+        {
+            if (context.HasItem<PileOfPlastic>())
+                return new PositiveInteractionResult("The boat must be on the ground to be deflated. ");
+
+            IsInflated = false;
+            return new PositiveInteractionResult("The boat deflates. ");
         }
 
         return base.RespondToSimpleInteraction(action, context, client);
@@ -59,7 +85,10 @@ public class PileOfPlastic : ItemBase, ICanBeTakenAndDropped
             return new PositiveInteractionResult("Inflating it further would probably burst it. ");
 
         IsInflated = true;
+        ItemPlacedHere(Repository.GetItem<Label>());
         return new PositiveInteractionResult(
-            "The boat inflates and appears seaworthy. A tan label is lying inside the boat. ");
+            "The boat inflates and appears seaworthy. " + (Repository.GetItem<Label>().CurrentLocation == this
+                ? "\nA tan label is lying inside the boat. "
+                : ""));
     }
 }
