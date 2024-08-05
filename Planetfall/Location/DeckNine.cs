7c50c3d19ba7b1e99471101212cbac485ef89015
Michael Lane <hello@newzork.ai>
2024-08-04 20:29:22 -0500

    WIP

--- Planetfall/Location/DeckNine.cs
+++ Planetfall/Location/DeckNine.cs
@@ -3,6 +3,7 @@ using GameEngine.Location;
 using Model.AIGeneration;
 using Model.Interface;
 using Model.Movement;
+using Planetfall.Command;
 using Planetfall.Item;
 
 namespace Planetfall.Location;
@@ -24,8 +25,33 @@ public class DeckNine : BaseLocation, ITurnBasedActor
         {
             Repository.GetItem<BulkheadDoor>().IsOpen = true;
             return Task.FromResult(
-                $" A massive explosion rocks the ship. Echoes from the explosion resound deafeningly down the halls. " +
+                $"\nA massive explosion rocks the ship. Echoes from the explosion resound deafeningly down the halls. " +
                 $"{ (context.CurrentLocation == this ?  "The door to port slides open." : "")}");
+        }
+
+        if (context.Moves == 11)
+        {
+            return Task.FromResult<string>("\nMore distant explosions! A narrow emergency bulkhead at the base of the " +
+                                           "gangway and a wider one along the corridor to starboard both crash shut!");
+        }
+        
+        if (context.Moves == 12)
+        {
+            Repository.GetItem<BulkheadDoor>().IsOpen = false;
+            return Task.FromResult(
+                $"\nMore powerful explosions buffet the ship. The lights flicker madly" +
+                $"{ (context.CurrentLocation == this ?  ", and the escape-pod bulkhead clangs shut" : "")}. ");
+        }
+        
+        if (context.Moves == 13)
+        {
+            return Task.FromResult<string>("\nExplosions continue to rock the ship. ");
+        }
+        
+        if (context.Moves == 14)
+        {
+            var result = "An enormous explosion tears the walls of the ship apart. If only you had made it to an escape pod...";
+            return Task.FromResult(new DeathProcessor().Process(result, context).InteractionMessage);
         }
 
         return Task.FromResult<string>("");
