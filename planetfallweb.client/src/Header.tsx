e2989be7597bd415d5b2211ef4e2f1690c92fb17
Michael Lane <mike@mikeindallas.com>
2024-08-11 16:41:40 -0500

    UI fixes

--- planetfallweb.client/src/Header.tsx
+++ planetfallweb.client/src/Header.tsx
@@ -8,8 +8,8 @@ interface HeaderComponentProps {
 
 const HeaderComponent: React.FC<HeaderComponentProps> = ({locationName, moves, score}) => (
     <div className="grid items-center bg-sky-950 p-3 text-white grid-cols-5 bebas-neue-regular text-2xl ">
-        <div className="col-span-3">{locationName}</div>
-        <div>Moves: {moves}</div>
+        <div className="col-span-2">{locationName}</div>
+        <div className="col-span-2">Moves: {moves}</div>
         <div>Score: {score}</div>
     </div>
 );
