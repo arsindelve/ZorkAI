535f089e6c6c904c108a9042b62c2aa59dbfd65a
arsindelve <mike@arsindelve.com>
2024-10-15 20:33:38 -0500

    You can give stuff to the troll now!

---
+++ GameEngine/IntentEngine/GiveSomethingToSomeoneDecisionEngine.cs
@@ -0,0 +1,43 @@
+﻿using GameEngine.Item;
+using Model.Interface;
+using Model.Item;
+
+namespace GameEngine.IntentEngine;
+
+/// <summary>
+///     Given an intent and a recipient, is it my intention with this text input
+///     to give something to the recipient?
+/// </summary>
+/// <typeparam name="TRecipient"></typeparam>
+public class GiveSomethingToSomeoneDecisionEngine<TRecipient> where TRecipient : ItemBase, ICanBeGivenThings, new()
+{
+    public InteractionResult? AreWeGivingSomethingToSomeone(MultiNounIntent action, TRecipient recipient,
+        IContext context)
+    {
+        if (!action.MatchVerb(Verbs.GiveVerbs))
+            return null;
+
+        // Recipient can be in either noun...."give thing to recipient" or "offer recipient the thing"
+        string? otherThingNoun;
+        if (action.MatchNounOne(recipient.NounsForMatching))
+        {
+            otherThingNoun = action.NounTwo;
+        }
+        else
+        {
+            if (action.MatchNounTwo(recipient.NounsForMatching))
+                otherThingNoun = action.NounOne;
+            else
+                return null;
+        }
+
+        var thing = Repository.GetItem(otherThingNoun);
+        if (thing is null)
+            return null;
+
+        if (!context.Items.Contains(thing))
+            return new PositiveInteractionResult($"You don't have the {thing.NounsForMatching[0]}! ");
+
+        return recipient.OfferThisThing(thing, context);
+    }
+}
---
+++ Model/Item/ICanBeGivenThings.cs
@@ -0,0 +1,13 @@
+﻿using Model.Interaction;
+using Model.Interface;
+
+namespace Model.Item;
+
+/// <summary>
+///     Marker interface for objects that can handed/given/offered things. Almost
+///     always this is a person/monster/robot/etc. 
+/// </summary>
+public interface ICanBeGivenThings
+{
+    InteractionResult OfferThisThing(IItem item, IContext context);
+}
