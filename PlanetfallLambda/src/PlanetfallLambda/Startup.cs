930d2bdf7aee3e76f398a62fb8ecf8ed212a6fde
arsindelve <mike@arsindelve.com>
2024-10-17 20:39:03 +0000

    Now we're getting somewhere

---
+++ PlanetfallLambda/src/PlanetfallLambda/Startup.cs
@@ -0,0 +1,56 @@
+﻿using DynamoDb;
+using GameEngine;
+using Model.AIGeneration;
+using Model.Interface;
+using OpenAI;
+using Planetfall;
+using ZorkOne;
+
+namespace Lambda;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddLogging();
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddSingleton<ISessionRepository, DynamoDbSessionRepository>();
+        services.AddScoped<IGameEngine, GameEngine<PlanetfallGame, PlanetfallContext>>();
+        services.AddScoped<ISavedGameRepository, DynamoDbSavedGameRepository>();
+        services.AddScoped<IGenerationClient, ChatGPTClient>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseCors(b => b
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+        app.UseSwagger();
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapGet("/",
+                async context =>
+                {
+                    await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
+                });
+        });
+    }
+}
