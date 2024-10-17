5c48914142b941aea59d55ecdd9c40c38b08dc00
arsindelve <mike@arsindelve.com>
2024-10-17 20:39:01 +0000

    A few more little fixes

---
+++ ZorkOne/Item/Label.cs
@@ -0,0 +1,29 @@
+namespace ZorkOne.Item;
+
+public class Label : ItemBase, ICanBeExamined, ICanBeRead, ICanBeTakenAndDropped
+{
+    public override string[] NounsForMatching => ["label", "tan label"];
+    public string ExaminationDescription => ReadDescription;
+
+    public string ReadDescription => """
+                                     !!!!FROBOZZ MAGIC BOAT COMPANY!!!!
+                                      
+                                     Hello, Sailor!
+                                      
+                                     Instructions for use:
+                                      
+                                        To get into a body of water, say "Launch".
+                                        To get to shore, say "Land" or the direction in which you want to maneuver the boat.
+                                      
+                                     Warranty:
+                                      
+                                       This boat is guaranteed against all defects for a period of 76 milliseconds from date of purchase or until first used, whichever comes first.
+                                      
+                                     Warning:
+                                        This boat is made of thin plastic.
+                                        Good Luck!
+                                     """;
+    public string OnTheGroundDescription => "There is a tan label here. ";
+
+    public override string InInventoryDescription => "A tan label";
+}
