3734f7d18279ee8cb5e26df97af2650f2a27872d
arsindelve <mike@arsindelve.com>
2024-10-17 20:39:03 +0000

    Fixes

--- planetfallweb.client/src/modal/WelcomeModal.tsx
+++ planetfallweb.client/src/modal/WelcomeModal.tsx
@@ -19,11 +19,11 @@ const WelcomeDialog: React.FC<WelcomeDialogProps> = ({open, handleClose}) => (
         aria-describedby="alert-dialog-description"
     >
         <DialogTitle id="alert-dialog-title">
-            {"Welcome to Zork AI"}
+            {"Welcome to Planetfall AI"}
         </DialogTitle>
         <DialogContent>
             <DialogContentText id="alert-dialog-description">
-                This is a re-imagining of the original 1980's classic adventure game Planetfall.
+                This is a re-imagining of the original 1980's classic adventure game Planetfall by Steve Meretzky.
                 The story is the same, but we've added AI enhanced parsing and response generation for
                 a more immersive experience. To get started, type your input in the grey box below, and press
                 'return'. Click the menu on the top left for more information. Enjoy!
