144f70d255f479df71786348ae78e38150a0f455
arsindelve <mike@arsindelve.com>
2024-10-17 20:39:01 +0000

    Maze fixes

--- ZorkOne/Item/Skeleton.cs
+++ ZorkOne/Item/Skeleton.cs
@@ -1,13 +1,15 @@
 namespace ZorkOne.Item;
 
-public class Skeleton : ItemBase
+public class Skeleton : ItemBase, ICanBeTakenAndDropped
 {
     public override string[] NounsForMatching => ["skeleton", "remains", "adventurer", "luckless adventurer"];
 
     public override string NeverPickedUpDescription =>
         "A skeleton, probably the remains of a luckless adventurer, lies here. ";
 
-    public override string? CannotBeTakenDescription =>
+    public string OnTheGroundDescription => NeverPickedUpDescription;
+    
+    public override string CannotBeTakenDescription =>
         "A ghost appears in the room and is appalled at your desecration of the remains of a fellow adventurer. " +
         //TODO: "He casts a curse on your valuables and banishes them to the Land of the Living Dead. " +
         "The ghost leaves, muttering obscenities. ";
