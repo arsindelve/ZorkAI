e080ccf5628e703a6a2688145a255ec87b6d9b62
Michael Lane <hello@newzork.ai>
2024-10-20 11:30:18 -0500

    Egg and canary fixes (#64)

--- ZorkOne/Item/Egg.cs
+++ ZorkOne/Item/Egg.cs
@@ -5,37 +5,46 @@ using Model.Interface;
 
 namespace ZorkOne.Item;
 
-public class Egg : ContainerBase, ICanBeTakenAndDropped, ICanBeExamined, IGivePointsWhenFirstPickedUp,
-    IGivePointsWhenPlacedInTrophyCase, IOpenAndClose
+public class Egg
+    : ContainerBase,
+        ICanBeTakenAndDropped,
+        ICanBeExamined,
+        IGivePointsWhenFirstPickedUp,
+        IGivePointsWhenPlacedInTrophyCase,
+        IOpenAndClose
 {
-    public override string[] NounsForMatching => ["egg", "jewel-encrusted egg", "jewel encrusted egg"];
+    public override string[] NounsForMatching =>
+        ["egg", "jewel-encrusted egg", "jewel encrusted egg"];
 
     public override int Size => 1;
 
     public bool IsDestroyed { get; set; }
 
-    public string ExaminationDescription => IsOpen
-        ? Items.Count == 0
-            ? $"The {(IsDestroyed ? "broken": "")} jewel-encrusted egg is empty. "
-            : Repository.GetItem<Canary>().HasEverBeenPickedUp || IsDestroyed
-                ? ItemListDescription($"{(IsDestroyed ? "broken": "")} jewel-encrusted egg", null)
-                : "There is a golden clockwork canary nestled in the egg. It has ruby eyes and a silver beak. Through a crystal window below its left wing you can see intricate machinery inside. It appears to have wound down. "
-        : $"The {(IsDestroyed ? "broken " : "")}jewel-encrusted egg is closed. ";
+    public string ExaminationDescription =>
+        IsOpen
+            ? Items.Count == 0
+                ? $"The {(IsDestroyed ? "broken" : "")} jewel-encrusted egg is empty. "
+                : Repository.GetItem<Canary>().HasEverBeenPickedUp || IsDestroyed
+                    ? ItemListDescription(
+                        $"{(IsDestroyed ? "broken" : "")} jewel-encrusted egg",
+                        null
+                    )
+                    : "There is a golden clockwork canary nestled in the egg. It has ruby eyes and a silver beak. Through a crystal window below its left wing you can see intricate machinery inside. It appears to have wound down. "
+            : $"The {(IsDestroyed ? "broken " : "")}jewel-encrusted egg is closed. ";
 
     public string OnTheGroundDescription(ILocation currentLocation)
     {
         return IsDestroyed
-            ? "There is a somewhat ruined egg here." + ItemListDescription("egg", null)
-            : "There is a jewel-encrusted egg here." + ItemListDescription("egg", null);
+            ? "There is a somewhat ruined egg here. " + ItemListDescription("egg", null)
+            : "There is a jewel-encrusted egg here. " + ItemListDescription("egg", null);
     }
 
     public override string NeverPickedUpDescription(ILocation currentLocation)
     {
-        return
-            "In the bird's nest is a large egg encrusted with precious jewels, apparently scavenged by a childless " +
-            "songbird. The egg is covered with fine gold inlay, and ornamented in lapis lazuli and mother-of-pearl. " +
-            "Unlike most eggs, this one is hinged and closed with a delicate looking clasp. The egg appears " +
-            "extremely fragile. ";
+        return "In the bird's nest is a large egg encrusted with precious jewels, apparently scavenged by a childless "
+            + "songbird. The egg is covered with fine gold inlay, and ornamented in lapis lazuli and mother-of-pearl. "
+            + "Unlike most eggs, this one is hinged and closed with a delicate looking clasp. The egg appears "
+            + "extremely fragile. ";
     }
 
     int IGivePointsWhenFirstPickedUp.NumberOfPoints => 5;
@@ -46,12 +55,9 @@ public class Egg : ContainerBase, ICanBeTakenAndDropped, ICanBeExamined, IGivePo
 
     public string NowOpen(ILocation currentLocation)
     {
-        var canary = Repository.GetItem<Canary>();
-
-        if (canary.IsDestroyed && canary.CurrentLocation == this)
-            return "Opening the broken jewel-encrusted egg reveals a broken clockwork canary. ";
-
-        return "Opened. ";
+        // You can never open it. If the thief opens it, you can close it, but never
+        // open it again, even if it's been damaged.
+        return string.Empty;
     }
 
     public string NowClosed(ILocation currentLocation)
@@ -67,15 +73,22 @@ public class Egg : ContainerBase, ICanBeTakenAndDropped, ICanBeExamined, IGivePo
 
     public string CannotBeOpenedDescription(IContext context)
     {
-        return IsDestroyed ? "" : "You have neither the tools nor the expertise. ";
+        return "You have neither the tools nor the expertise. ";
     }
 
-    public override InteractionResult RespondToMultiNounInteraction(MultiNounIntent action, IContext context)
+    public override InteractionResult RespondToMultiNounInteraction(
+        MultiNounIntent action,
+        IContext context
+    )
     {
         if (!action.MatchNounOne(NounsForMatching))
             return base.RespondToMultiNounInteraction(action, context);
 
-        if (!action.MatchVerb(["open", "pry", "break", "jimmie", "crack", "force", "bust", "pop", "split"]))
+        if (
+            !action.MatchVerb(
+                ["open", "pry", "break", "jimmie", "crack", "force", "bust", "pop", "split"]
+            )
+        )
             return base.RespondToMultiNounInteraction(action, context);
 
         if (action.MatchNounTwo(Repository.GetItem<Sword>().NounsForMatching))
@@ -90,7 +103,8 @@ public class Egg : ContainerBase, ICanBeTakenAndDropped, ICanBeExamined, IGivePo
         return base.RespondToMultiNounInteraction(action, context);
     }
 
-    private InteractionResult PryOpen<T>(IContext context) where T : IItem, new()
+    private InteractionResult PryOpen<T>(IContext context)
+        where T : IItem, new()
     {
         if (!context.HasItem<T>())
             return new PositiveInteractionResult("You don't have that! ");
@@ -100,8 +114,10 @@ public class Egg : ContainerBase, ICanBeTakenAndDropped, ICanBeExamined, IGivePo
         Repository.GetItem<Canary>().IsDestroyed = true;
 
         return new PositiveInteractionResult(
-            "The egg is now open, but the clumsiness of your attempt has seriously compromised " +
-            "its esthetic appeal. There is a golden clockwork canary nestled in the egg. " + Canary.DestroyedMessage);
+            "The egg is now open, but the clumsiness of your attempt has seriously compromised "
+                + "its esthetic appeal. There is a golden clockwork canary nestled in the egg. "
+                + Canary.DestroyedMessage
+        );
     }
 
     public override string ItemListDescription(string name, ILocation? location)
@@ -112,21 +128,20 @@ public class Egg : ContainerBase, ICanBeTakenAndDropped, ICanBeExamined, IGivePo
         var canary = Repository.GetItem<Canary>();
 
         if (Items.Contains(canary) && canary.IsDestroyed)
-            return
-                "\nThere is a golden clockwork canary nestled in the broken egg. " + Canary.DestroyedMessage;
+            return "\nThere is a golden clockwork canary nestled in the broken egg. "
+                + Canary.DestroyedMessage;
 
         if (Items.Contains(canary))
-            return
-                "\nThere is a golden clockwork canary nestled in the egg. It has ruby eyes and a silver beak. " +
-                "Through a crystal window below its left wing you can see intricate machinery inside. " +
-                "It appears to have wound down.";
+            return "\nThere is a golden clockwork canary nestled in the egg. It has ruby eyes and a silver beak. "
+                + "Through a crystal window below its left wing you can see intricate machinery inside. "
+                + "It appears to have wound down.";
 
         return base.ItemListDescription(name, location);
     }
 
     public override string GenericDescription(ILocation? currentLocation)
     {
-        return IsDestroyed ? "A broken jewel-encrusted egg": "A jewel-encrusted egg";
+        return IsDestroyed ? "A broken jewel-encrusted egg" : "A jewel-encrusted egg";
     }
 
     public override void Init()
