52b8c714ab42ab2d109ffc7df3bf1784a5c5941b
Michael Lane <hello@newzork.ai>
2024-12-10 21:52:58 -0600

    More PF locations

---
+++ Planetfall/Location/Kalamontee/PlainHall.cs
@@ -0,0 +1,42 @@
+﻿using GameEngine.Location;
+using Model.Movement;
+
+namespace Planetfall.Location.Kalamontee;
+
+internal class PlainHall : LocationWithNoStartingItems
+{
+    protected override Dictionary<Direction, MovementParameters> Map =>
+        new()
+        {
+            { Direction.S, Go<Courtyard>() }
+        };
+
+    protected override string ContextBasedDescription =>
+        "This is a featureless hall leading north and south. Although the hallway is old and dusty, " +
+        "the construction is of a much more modern style than the castle to the south. A similar " +
+        "hall branches off to the northeast. ";
+
+    public override string Name => "Plain Hall";
+}
+
+internal class RecArea : LocationWithNoStartingItems
+{
+    protected override Dictionary<Direction, MovementParameters> Map =>
+        new()
+        {
+            { Direction.S, Go<PlainHall>() }
+        };
+
+    // TODO: >examine games
+    // All the usual games -- Chess, Cribbage, Galactic Overlord, Double Fannucci...
+    
+    // TODO: >examine tapes
+    // Let's see...here are some musical selections, here are some bestselling romantic novels, here is a biography of a famous Double Fannucci champion...
+    
+    protected override string ContextBasedDescription =>
+        "This is a recreational facility of some sort. Games and tapes are scattered about the room. " +
+        "Hallways head off to the east and south, and to the north is a door which is closed and locked. " +
+        "A dial on the door is currently set to 0. ";
+    
+    public override string Name => "Rec Area";
+}
---
+++ Planetfall/Location/Kalamontee/WestWing.cs
@@ -0,0 +1,19 @@
+﻿using GameEngine.Location;
+using Model.Movement;
+
+namespace Planetfall.Location.Kalamontee;
+
+public class WestWing : LocationWithNoStartingItems
+{
+    protected override Dictionary<Direction, MovementParameters> Map =>
+        new()
+        {
+            { Direction.E, Go<Courtyard>() }
+        };
+
+    protected override string ContextBasedDescription =>
+        "This was once the west wing of the castle, but the walls are now mostly rubble, " +
+        "allowing a view of the cliff and ocean below. Rubble blocks all exits save one, eastward to the courtyard. ";
+
+    public override string Name => "West Wing";
+}
