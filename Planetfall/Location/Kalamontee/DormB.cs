0fa9fddc4646a13c1325fad88ca1e0797647f715
Michael Lane <mike@arsindelve.com>
2024-12-14 14:27:00 +0000

    WIP

---
+++ Planetfall/Location/Kalamontee/DormB.cs
@@ -0,0 +1,18 @@
+using GameEngine.Location;
+using Model.Movement;
+
+namespace Planetfall.Location.Kalamontee;
+
+internal class DormB : LocationWithNoStartingItems
+{
+    protected override Dictionary<Direction, MovementParameters> Map =>
+new()
+{
+            { Direction.S, Go<RecCorridor>() }
+};
+
+    protected override string ContextBasedDescription =>
+       "This is a very long room lined with multi-tiered bunks. Flimsy partitions between the tiers may have provided a modicum of privacy. These spartan living quarters could have once housed many hundreds, but it seems quite deserted now. There are openings at the north and south ends of the room.";
+
+    public override string Name => "Dorm B";
+}
