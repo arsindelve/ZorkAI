101c22dee5e4382653b273067634966fea54685a
Michael Lane <hello@newzork.ai>
2024-12-15 18:07:36 -0600

    WIP

---
+++ Planetfall/Item/Mech/Magnet.cs
@@ -0,0 +1,21 @@
+namespace Planetfall.Item.Mech;
+
+public class Magnet : ItemBase, ICanBeTakenAndDropped
+{
+    public override string[] NounsForMatching => ["curved metal bar", "metal bar", "bar", "magnet"];
+
+    public string OnTheGroundDescription(ILocation currentLocation)
+    {
+        return "There is a curved metal bar here. ";
+    }
+
+    public override string NeverPickedUpDescription(ILocation currentLocation)
+    {
+        return "On an upper shelf is a metal bar, curved into a U-shape. ";
+    }
+
+    public override string GenericDescription(ILocation? currentLocation)
+    {
+        return "A curved metal bar";
+    }
+}
