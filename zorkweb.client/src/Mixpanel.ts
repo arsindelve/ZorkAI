c1bbfcee87ab624f9650b4119124ca8733f3b38e
Michael Lane <hello@newzork.ai>
2024-12-20 17:36:18 -0600

    Fix

--- zorkweb.client/src/Mixpanel.ts
+++ zorkweb.client/src/Mixpanel.ts
@@ -1,5 +1,5 @@
 import {SessionHandler} from "./SessionHandler.ts";
-import mixpanel from 'mixpanel-browser';
+import mixpanel, { Dict } from 'mixpanel-browser';
 
 let sessionId = new SessionHandler();
 mixpanel.init('62af8d500a6f295a71ef335b6ff56942');
@@ -8,19 +8,11 @@ mixpanel.identify(sessionId.getClientId());
 let env_check = true;
 
 let actions = {
-    identify: (id) => {
+    identify: (id: string) => {
         if (env_check) mixpanel.identify(id);
     },
-    alias: (id) => {
-        if (env_check) mixpanel.alias(id);
-    },
-    track: (name, props) => {
+    track: (name: string, props: Dict | undefined) => {
         if (env_check) mixpanel.track(name, props);
-    },
-    people: {
-        set: (props) => {
-            if (env_check) mixpanel.people.set(props);
-        },
     },
 };
 
