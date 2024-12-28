715cccf67eb439aca5613863fa1e0fa6aa5ecca5
Michael Lane <hello@newzork.ai>
2024-12-28 13:45:58 -0600

    Ambassador is excellent!

--- Planetfall/Item/QuirkyCompanion.cs
+++ Planetfall/Item/QuirkyCompanion.cs
@@ -13,8 +13,7 @@ internal abstract class QuirkyCompanion : ContainerBase, ITurnBasedActor
                                            Come up with something to say or do that is funny, but consistent 
                                            with what this character would be expected to do, 
                                            and which has absolutely no effect on the state of the 
-                                           game. Don't provide any quotes around the text. Provide two 
-                                           newlines at the end. 
+                                           game. Don't provide any quotes around the text. 
                                            """;
 
     protected async Task<string> GenerateCompanionSpeech(IContext context, IGenerationClient client)
