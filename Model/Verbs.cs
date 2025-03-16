c17712c4a22c57e54e26c7b91a882fe1399a2fd5
Michael Lane <mike@arsindelve.com>
2025-03-16 12:28:37 -0500

    Stable

--- Model/Verbs.cs
+++ Model/Verbs.cs
@@ -12,7 +12,7 @@ public static class Verbs
     public static readonly string[] MoveVerbs = ["move", "go", "walk", "run", "travel", "proceed", "head", "stroll", "escape"];
     public static readonly string[] KillVerbs = ["kill", "attack", "defeat", "destroy", "murder", "stab"];
     public static readonly string[] CloseVerbs = ["close", "shut", "bar", "lower"];
-    public static readonly string[] OpenVerbs = ["open", "lift"];
+    public static readonly string[] OpenVerbs = ["open", "lift", "pry", "unbar", "raise"];
     public static readonly string[] GiveVerbs = ["give", "offer", "transfer", "present", "provide"];
     public static readonly string[] ThrowVerbs = ["throw", "toss", "launch", "hurl", "fling", "heave", "chuck", "lob"];
     public static readonly string[] SayVerbs = ["say", "yell", "shout", "utter", "scream", "mumble", "whisper", "speak", "declare", "state", "announce"];
