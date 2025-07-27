59ba0ce6fbd6d122b6ca47eb6cb17c417adb209b
Michael Lane <mike@arsindelve.com>
2025-07-26 19:55:10 -0500

    Add ICanBeTalkedTo interface and apply to NPCs

---
+++ Model/Item/ICanBeTalkedTo.cs
@@ -0,0 +1,8 @@
+using Model.Interface;
+
+namespace Model.Item;
+
+/// <summary>
+/// Marker interface for items that can be the target of a conversation.
+/// </summary>
+public interface ICanBeTalkedTo : IInteractionTarget;
