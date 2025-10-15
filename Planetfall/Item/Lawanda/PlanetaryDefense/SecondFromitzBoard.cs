2a853ba5190c5d665c2822a8e00dfcc49457d64f
Michael Lane <hello@newzork.ai>
2025-10-15 13:13:08 -0500

    Add Fromitz boards and access panel to Planetary Defense location

---
+++ Planetfall/Item/Lawanda/PlanetaryDefense/SecondFromitzBoard.cs
@@ -0,0 +1,14 @@
+﻿namespace Planetfall.Item.Lawanda.PlanetaryDefense;
+
+public class SecondFromitzBoard : FromitzBoardBase
+{
+    public override string[] NounsForMatching =>
+    [
+        "fromitz board", "board", "fromitz", "second", "second fromitz board", "second board", "second fromitz"
+    ];
+    
+    public override string GenericDescription(ILocation? currentLocation)
+    {
+        return "A second seventeen-centimeter fromitz board ";
+    }
+}
