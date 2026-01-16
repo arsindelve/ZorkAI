95daec7c257486bff3be366807b2b9753d9a89ee
Michael Lane <mike@mikeindallas.com>
2026-01-16 13:29:36 -0600

    - Standardized character dialogue formatting across multiple scripts. - Changed GameEngine configuration to enable generated responses.

--- Planetfall/Item/QuirkyCompanion.cs
+++ Planetfall/Item/QuirkyCompanion.cs
@@ -13,25 +13,27 @@ public abstract class QuirkyCompanion : ContainerBase, ITurnBasedActor
 
     protected virtual string UserPrompt => """
                                            You and the player are in this room "{0}" which has this description: "{1}".
-                                           
-                                           ### **Recent Context (Last 5 Things you have Said or Done):**  
-                                           Here are the last 5 things that you have said or done (most recent first): 
-                                           
+
+                                           ### **Recent Context (Last 5 Things you have Said or Done):**
+                                           Here are the last 5 things that you have said or done (most recent first):
+
                                            {2}
-                                           
+
                                            ---
-                                           
-                                           Come up with something to say or do that is funny, but consistent 
-                                           with what this character would be expected to do, 
-                                           and which has absolutely no effect on the state of the 
+
+                                           Come up with something to say or do that is funny, but consistent
+                                           with what this character would be expected to do,
+                                           and which has absolutely no effect on the state of the
                                            game. Do not offer or give anything to the player. Do not
-                                           request that the player do anything that might change the 
+                                           request that the player do anything that might change the
                                            state of the game in any way, or require them to leave their
                                            current location. Do not leave the current location.
                                            If possible, make connections with the previous things you have said
                                            and done in order to provide some continuity.
 
-                                           Don't provide any quotes around the text. 
+                                           IMPORTANT: When your character speaks, always include an attribution
+                                           (like "says", "remarks", "screams", etc.) and put quotes around the spoken text.
+                                           Never output unattributed speech without quotes.
                                            """;
 
     public abstract Task<string> Act(IContext context, IGenerationClient client);
